be2755498c104a1e3dbb269c682670af6e34bda1
Calum-Johnston <calumphilipjohnston@gmail.com>
2020-01-06 11:09:38 +0000

    Added Javadoc formatted comments, removed main method, added ability to get new element

--- pseudorandom.java
+++ pseudorandom.java
@@ -3,38 +3,66 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * The class produces sequence of unique numbers within a set
+ * range based off a key.
+ * The key is some user input (typically a password).
+ * 
+ * Designed for Image Steganography
+ */
 public class pseudorandom{
 
     int height;
     int width;
     String seed;
-    List<Integer> orderedSequence = new ArrayList<Integer>();
-    List<Integer> randomSequence = new ArrayList<Integer>();
+    List<Integer> orderSequence;
+    int position;
 
+    /**
+     * Secondary Constructor for the class
+     * Only called when no user-input given
+     * 
+     * @param height    the height of the image
+     * @param width     the width of the image
+     */
     public pseudorandom(int height, int width) {
         this(height, width, "default");
     }
 
+    /**
+     * Primary Constructor for the class
+     * 
+     * @param height    the height of the image
+     * @param width     the width of the image
+     * @param seed      the seed to be used for random generation
+     */
     public pseudorandom(int height, int width, String seed){
         this.height = height;
         this.width = width;
         this.seed = seed;
+        orderSequence = new ArrayList<Integer>();
+        position = -1;
     }
 
+    /**
+     * Generates the random sequence to be used
+     */
     public void generateRandomList(){
         for(int i = 0; i < width * height; i++){
-            orderedSequence.add(i);
+            orderSequence.add(i);
         }
         // https://stackoverflow.com/questions/6284589/setting-a-seed-to-shuffle-arraylist-in-java-deterministically
         // https://stackoverflow.com/questions/27346809/getting-a-range-off-user-input-for-random-generation
-        Collections.shuffle(orderedSequence, new Random(seed.hashCode()));
+        Collections.shuffle(orderSequence, new Random(seed.hashCode()));
     }
 
-            
-    public static void main(String[] args) {
-        pseudorandom test = new pseudorandom();
-        for(int i = 0; i < 100; i++){
-            System.out.println(test.generateNewNumber() % 1000000);
-        }
+    /**
+     *  Gets the next element in the random sequence
+     * 
+     * @return      the next element in the sequence
+     */
+    public int getNextElement(){
+        position += 1;
+        return orderSequence.get(position);
     }
 }
---
+++ readme.md
@@ -0,0 +1,8 @@
+A pseudo random number generator that generates a unique sequence of numbers
+within some defined range based on a user-defined seed.
+
+- Seed = Hash of some user input
+- Range = The size of an image (can be changed)
+
+Originally designed for use with Steganography in generating a unique order 
+to visit pixels when encoding data.
